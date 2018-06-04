509c069093a88c5bc58dc9bcc3310bba018981f4
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-04 07:35:34 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif 40 < speed < 60
+		return false
+	else
+		nil
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
